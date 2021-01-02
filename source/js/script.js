2c5f6069df62a31327ec59dac5da0db9627bb4a2
sashkatenev <tel256-32-79@yandex.ru>
2021-01-02 08:56:55 +0300

    sets styles to decorative elements of header

---
+++ source/js/script.js
@@ -0,0 +1,19 @@
+var siteNavigation = document.querySelector(".site-navigation");
+var siteNavigationToggleButton = document.querySelector(".site-navigation__toggler");
+
+if (siteNavigation) {
+  siteNavigation.classList.add("site-navigation--closed");
+
+  if (siteNavigationToggleButton) {
+    siteNavigationToggleButton.removeAttribute("disabled");
+
+    siteNavigationToggleButton.addEventListener("click", function (evt) {
+      if (siteNavigation.classList.contains("site-navigation--closed")) {
+        siteNavigation.classList.remove("site-navigation--closed");
+      }
+      else {
+        siteNavigation.classList.add("site-navigation--closed");
+      }
+    });
+  }
+}
